7053a41726e096d5aaa5d53480ef37311b159561
Chayabk12 <44639103+Chayabk12@users.noreply.github.com>
2018-11-14 23:41:18 +0530

    Add files via upload

---
+++ armstrong_betwn.java
@@ -0,0 +1,29 @@
+package assign_java;
+
+public class armstrong_betwn {
+	             public static void main (String [] args) 
+	             { 
+
+
+	                    for (int num = 100 ; num <= 1000 ; num ++) 
+	                     { 
+	                                int n = num; 
+	                                int temp = 0; 
+	                                int res = 0;
+
+	                                while (n > 0) 
+	                                 { 
+	                                             temp= n % 10; 
+	                                             res = res + (temp * temp * temp); 
+	                                             n = n / 10; 
+	                                 } 
+	                                 if (num == res) 
+	                                  { 
+	                                             System.out.println (num); 
+	                                   } 
+	                      } 
+	            } 
+	}
+	
+
+
